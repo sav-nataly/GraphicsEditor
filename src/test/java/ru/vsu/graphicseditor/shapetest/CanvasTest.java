3b910c9f9735675426b21e8b0cfa570b43fb82b4
sav-nataly <natali.savina.01@mail.ru>
2020-10-18 11:33:19 +0300

    Tools added

--- src/test/java/ru/vsu/graphicseditor/shapetest/CanvasTest.java
+++ src/test/java/ru/vsu/graphicseditor/shapetest/CanvasTest.java
@@ -16,13 +16,13 @@ public class CanvasTest {
     public void canvasTest(){
         Canvas canvas = new Canvas(700, 700, Color.WHITE);
 
-        assertEquals(canvas.getLayerList().size(), 1);
+        assertEquals(1, canvas.getLayerList().size());
 
         canvas.addNewLayer();
-        assertEquals(canvas.getLayerList().get(1).getName(), "Layer1");
+        assertEquals("Layer1", canvas.getLayerList().get(1).getName());
 
         canvas.moveLayer(1, 0);
-        assertEquals(canvas.getLayerList().get(0).getName(), "Layer1");
+        assertEquals("Layer1", canvas.getLayerList().get(0).getName());
 
         canvas.addNewLayer();
         canvas.addNewLayer();
@@ -33,35 +33,36 @@ public class CanvasTest {
         Line line = new Line(120, 120, 30, 30, Color.BLACK, Color.BLACK, 2);
         canvas.addShapeLayer(AddLineTool.add(line, canvas.getCurrentLayerNumber() + 1));
         canvas.renameLayer(3, "Line1");
-        assertEquals(canvas.getLayerList().get(3).getShapes().get(0).toString(), "Line pointList=[java.awt.Point[x=120,y=120], " +
-                "java.awt.Point[x=30,y=30]]borderColor=java.awt.Color[r=0,g=0,b=0]color=java.awt.Color[r=0,g=0,b=0]stroke=2");
-        assertEquals(canvas.getLayerList().get(3).getName(), "Line1");
+        assertEquals("Line pointList=[java.awt.Point[x=120,y=120], " +
+                "java.awt.Point[x=30,y=30]]borderColor=java.awt.Color[r=0,g=0,b=0]color=java.awt.Color[r=0,g=0,b=0]stroke=2",
+                canvas.getLayerList().get(3).getShapes().get(0).toString());
+        assertEquals("Line1", canvas.getLayerList().get(3).getName());
 
         canvas.addShapeLayer(AddPolygonTool.add(new Polygon(Arrays.asList(new Point(150, 0),
                 new Point(150, 80), new Point(150, 40)), Color.red, Color.red, 2),
                 canvas.getCurrentLayerNumber() + 1));
         canvas.combineLayers(3, 4);
-        assertEquals(canvas.getCurrentLayer().getShapes().get(1).getColor(), Color.red);
+        assertEquals(Color.red, canvas.getCurrentLayer().getShapes().get(1).getColor());
 
         BucketTool.color(50, 50, canvas.getCurrentLayer(), Color.CYAN);
-        assertEquals(canvas.getLayerList().get(3).getShapes().get(0).getColor(), Color.CYAN);
+        assertEquals(Color.CYAN, canvas.getLayerList().get(3).getShapes().get(0).getColor());
 
         BucketTool.color(0, 0, canvas.getCurrentLayer(), Color.GREEN);
-        assertEquals(canvas.getLayerList().get(3).getShapes().get(0).getColor(), Color.CYAN);
-        assertEquals(canvas.getCurrentLayer().getShapes().get(1).getColor(), Color.red);
-        assertEquals(canvas.getLayerList().get(3).getBackgroundColor(), Color.GREEN);
+        assertEquals(Color.CYAN, canvas.getLayerList().get(3).getShapes().get(0).getColor());
+        assertEquals(Color.red, canvas.getCurrentLayer().getShapes().get(1).getColor());
+        assertEquals(Color.GREEN, canvas.getLayerList().get(3).getBackgroundColor());
 
         DeleteTool.deleteShape(50, 50, canvas.getCurrentLayer().getShapes());
-        assertEquals(canvas.getCurrentLayer().getShapes().size(), 1);
+        assertEquals(1, canvas.getCurrentLayer().getShapes().size());
 
         canvas.deleteLayer(0);
-        assertEquals(canvas.getLayerList().size(), 5);
+        assertEquals(5, canvas.getLayerList().size());
 
         canvas.deleteLayer(0);
         canvas.deleteLayer(0);
         canvas.deleteLayer(0);
         canvas.deleteLayer(0);
         canvas.deleteLayer(0);
-        assertEquals(canvas.getLayerList().size(), 1);
+        assertEquals(1, canvas.getLayerList().size());
     }
 }
--- src/test/java/ru/vsu/graphicseditor/shapetest/ShapeTest.java
+++ src/test/java/ru/vsu/graphicseditor/shapetest/ShapeTest.java
@@ -25,41 +25,6 @@ public class ShapeTest {
 
         assertEquals(2, polyline.getPointList().size());
         assertEquals(2, polyline.getNPoints());
-        assertEquals("PrimitivePoint x=3, y=4", polyline.getPoint(1).toString());
+        assertEquals("java.awt.Point[x=3,y=4]", polyline.getPoint(1).toString());
     }
-
-//    @Test
-//    public void canvasTest(){
-//        Canvas canvas = new Canvas();
-//        ArrayList<Point> pointArrayList = new ArrayList<>();
-//        pointArrayList.add(new Point(3, 4));
-//        pointArrayList.add(new Point(5, 6));
-//        pointArrayList.add(new Point(7, 4));
-//
-//        ArrayList<java.awt.Point> pointArrayList2 = new ArrayList<>();
-//        pointArrayList2.add(new Point(0, 0));
-//        pointArrayList2.add(new Point(2, 3));
-//        pointArrayList2.add(new Point(3, 0));
-//
-//        List<Shape> shapes = new ArrayList<>();
-//        shapes.add(new Polyline(pointArrayList));
-//        shapes.add(new Polygon(pointArrayList2));
-//        shapes.add(new Line());
-//
-//        canvas.setShapeList(shapes);
-//
-//        canvas.getShapeList().remove(2);
-//        assertEquals(2, canvas.getShapeList().size());
-//
-//        Shape shape = canvas.getShape(5, 5);
-//        assertEquals("Polyline nPoints=3 pointList=[java.awt.Point[x=3,y=4], " +
-//                "java.awt.Point[x=5,y=6], java.awt.Point[x=7,y=4]] borderColor=null color=null stroke=0", shape.toString());
-//
-//        canvas.deleteShape(2, 2);
-//        assertEquals(1, canvas.getShapeList().size());
-//
-//        canvas.addShape(new Line());
-//        assertEquals(2, canvas.getShapeList().size());
-
-//    }
 }
